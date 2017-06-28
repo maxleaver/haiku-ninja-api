26c29bc6b8cbf5e1dd6067ffddf4fa2f3181edf4
Max Leaver <maxleaver@gmail.com>
2017-06-28 17:16:56 -0400

    Added environment variables to test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,11 @@ date_default_timezone_set('UTC');
 define('PROJECT_ROOT', realpath(__DIR__ . '/..'));
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Load env variables
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
 require __DIR__ . '/../tests/LocalWebTestCase.php';
 
 /* End of file bootstrap.php */
