a7a4b328fa5b7775c7c4098e227cc984e8510938
Max Leaver <maxleaver@gmail.com>
2017-06-29 14:46:25 -0400

    Updated php-haiku library and updated settings so error details display in development environments

--- app/settings.php
+++ app/settings.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => false, // set to false in production
+        'displayErrorDetails' => getenv('APP_ENV') !== 'production', // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Monolog settings
         'logger' => [
             'name' => getenv('APP_NAME'),
             'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+            'level' => \Monolog\Logger::DEBUG
+        ]
+    ]
 ];
